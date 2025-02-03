697f69566d7e4ca8ef92fcc1f78662fa2a8a7d21
mandar <sakpalm466@gmail.com>
2025-02-03 19:04:17 +0530

    implemented logout btn

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,36 @@
 import { Link, useLocation } from "react-router-dom";
 import tasksLogo from "../assets/tasks.webp";
+import { useUpdateUser, useUser } from "../context/User/userContext";
 const Header = () => {
+  const updateUser  = useUpdateUser();
+  const { username } = useUser();
   const location = useLocation(); // Get current route
   return (
     <nav className="w-full h-[50px] shadow-md fixed bg-white top-0 px-[2vw] flex items-center justify-between ">
-      <Link to="/" className="w-[20px] h-[20px] md:w-[25px] md:h-[25px] flex items-center gap-2">
+      <Link
+        to="/"
+        className="w-[20px] h-[20px] md:w-[25px] md:h-[25px] flex items-center gap-2"
+      >
         <img className="w-full h-full " src={tasksLogo} alt="TaskLy" />
-        <p className="font-bold text-sky-600 md:text-[25px] text-[20px] ">Taskly</p>
+        <p className="font-bold text-sky-600 md:text-[25px] text-[20px] ">
+          Taskly
+        </p>
       </Link>
-      {
-        // dont show login or logout button on login and signin page 
-        (location.pathname !== "/login") &&
-        <div>
-        <Link to="/login">
-          <button className="btn btn-info btn-xs md:btn-sm  text-white text-[20px] outline-0">
-            Login In
-          </button>
-        </Link>
-      </div>
-      }
+      {username && username.length > 0 ? (
+        <button onClick={()=>updateUser("")} className="btn btn-info btn-xs md:btn-sm  text-white text-[20px] outline-0">
+          Log out
+        </button> // dont show login or logout button on login and signin page
+      ) : (
+        location.pathname !== "/login" && (
+          <div>
+            <Link to="/login">
+              <button className="btn btn-info btn-xs md:btn-sm  text-white text-[20px] outline-0">
+                Login In
+              </button>
+            </Link>
+          </div>
+        )
+      )}
     </nav>
   );
 };
